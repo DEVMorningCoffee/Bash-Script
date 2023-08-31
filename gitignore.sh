bf103852a13436373e2b1f9c241e8ebacbd4820f
Jeffrey Abraham <JeffreyAbraham27@gmail.com>
2023-08-31 16:55:30 -0400

    Use Toptal Api to fetch gitignore template

--- gitignore.sh
+++ gitignore.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-
 # Get Ignore File
-
-function gitignore(){
-	case $1 in
-		Python | py | python)
-			CURLHTTP='https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore'
-			;;
-
-		C++ |c++ | cpp)
-			CURLHTTP='https://raw.githubusercontent.com/github/gitignore/main/C%2B%2B.gitignore'
-			;;
-
-		*)
-			exit 0
-			;;
-
-	esac
-
-	curl $CURLHTTP > "./.gitignore"
-}
-
-
-
+echo "function gi() { curl -sL https://www.toptal.com/developers/gitignore/api/\$@ ;}" >> \
+~/.bashrc && source ~/.bashrc
